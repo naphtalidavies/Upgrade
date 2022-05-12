cc8fbcf792dd4701eecc237173e640c986a3c4b3
naphtalidavies <naphtali.davies@gmail.com>
2022-05-12 12:50:30 +0300

    Update upgrade.bash

--- upgrade.bash
+++ upgrade.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-LOG_DIR=/var/log/zerto/containers/upgrade
-
 TAG=9.5.10.3777
 MANAGEMENT_CONSOLE_TAG=1.0.97
+REGISTRY='zartifactory.zerto.local:8081/zvm-docker-registry/release/jameson_u1'
 VERSION=$(echo $TAG | rev | cut -d"." -f2-  | rev)
- echo $VERSION
 LOG_FILE=$VERSION.log
+LOG_DIR=/var/log/zerto/containers/upgrade
 
 sudo mkdir -p $LOG_DIR
+
 log(){    
 
      echo -e "$(date) $1" | sudo tee -a "$LOG_DIR/$LOG_FILE"
@@ -16,10 +16,10 @@ log(){
 }
 
 log "Replacing registry path"
-sed -i 's|localhost:5000|zartifactory.zerto.local:8081/zvm-docker-registry/release/jameson_u1|g' docker-compose.local.yml
+sed -i 's|localhost:5000|'$REGISTRY'|g' /opt/zerto/zvr/docker-compose.local.yml
 
 log "About to upgrade containers to "$VERSION 
-TAG=$TAG MANAGEMENT_CONSOLE_TAG=$MANAGEMENT_CONSOLE_TAG docker-compose -f /home/naphtali/zlinux/zvm/docker-compose.local.yml -f /home/naphtali/zlinux/zvm/docker-compose.yml --env-file /home/naphtali/zlinux/zvm/.env up -d
+TAG=$TAG MANAGEMENT_CONSOLE_TAG=$MANAGEMENT_CONSOLE_TAG docker-compose -f /opt/zerto/zvr/docker-compose.local.yml -f /opt/zerto/zvr/docker-compose.yml --env-file /opt/zerto/zvr/.env up -d
 
 log "Finshied updgrade containers of version "$VERSION 
 
